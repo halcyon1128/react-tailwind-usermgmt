2df8a33fd793a960cbb26ac41a1125d315631ebd
halcyon1128 <acgorospeofficial@gmail.com>
2024-09-15 11:44:45 +0800

    refactored ArrayDatabase with BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,35 +1,59 @@
-import React from 'react';
+import React from 'react'
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-} from 'react-router-dom';
-import Login from './components/Login';
-import Users from './components/Users';
-import Edit from './components/Edit';
-import Settings from './components/Settings';
-import AddUser from './components/AddUser';
-import Table from './components/Table';
-import { AuthProvider, useAuth } from './components/contexts/AuthContext'; // Import the AuthProvider
-import { UserProvider } from './components/contexts/UserContext'; // Import the UserProvider
-import { AdminProvider } from './components/contexts/AdminContext';
-import ArrayDatabase from './components/ArrayDatabase';
+  BrowserRouter,
+} from 'react-router-dom'
+import Login from './components/Login'
+import Users from './components/Users'
+import Edit from './components/Edit'
+import Settings from './components/Settings'
+import AddUser from './components/AddUser'
+import Table from './components/Table'
+import { AuthProvider, useAuth } from './components/contexts/AuthContext' // Import the AuthProvider
+import { UserProvider } from './components/contexts/UserContext' // Import the UserProvider
+import { AdminProvider } from './components/contexts/AdminContext'
+import ArrayDatabase from './components/ArrayDatabase'
 
 function AppRoutes() {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn } = useAuth()
+  const baseURL = process.env.PUBLIC_URL;
+  console.log('baseURL: ', baseURL);
 
   return (
-    <Routes>
-      <Route exact path="/login" element={!isLoggedIn ? <Login /> : <Navigate to="/users" />} />
-      <Route path="/users" element={isLoggedIn ? <Users /> : <Navigate to="/login" />} />
-      <Route path="/edit/:id" element={isLoggedIn ? <Edit /> : <Navigate to="/login" />} />
-      <Route path="/settings" element={isLoggedIn ? <Settings /> : <Navigate to="/login" />} />
-      <Route path="/adduser" element={isLoggedIn ? <AddUser /> : <Navigate to="/login" />} />
-      <Route path="/table" element={isLoggedIn ? <Table /> : <Navigate to="/login" />} />
-      <Route path="*" element={<Navigate to={isLoggedIn ? "/users" : "/login"} />} />
-    </Routes>
-  );
+
+    <BrowserRouter basename={baseURL}>
+
+      <Routes>
+
+        <Route exact path={`${baseURL}/login`} element={!isLoggedIn ? <Login /> : <Navigate to="/users" />} />
+        <Route
+          path={`${baseURL}/users`}
+          element={isLoggedIn ? <Users /> : <Navigate to="/login" />}
+        />
+        <Route
+          path={`${baseURL}/edit/:id`}
+          element={isLoggedIn ? <Edit /> : <Navigate to="/login" />}
+        />
+        <Route
+          path={`${baseURL}/settings`}
+          element={isLoggedIn ? <Settings /> : <Navigate to="/login" />}
+        />
+        <Route
+          path={`${baseURL}/adduser`}
+          element={isLoggedIn ? <AddUser /> : <Navigate to="/login" />}
+        />
+        <Route
+          path={`${baseURL}/table`}
+          element={isLoggedIn ? <Table /> : <Navigate to="/login" />}
+        />
+        <Route path="*" element={<Navigate to={isLoggedIn ? `${baseURL}/users` : `${baseURL}/login`} />} />
+      </Routes>
+
+    </BrowserRouter>
+  )
 }
 
 function App() {
@@ -38,14 +62,14 @@ function App() {
       <AdminProvider>
         <AuthProvider>
           <UserProvider>
-            <Router basename={process.env.PUBLIC_URL}>
+            <Router>
               <AppRoutes />
             </Router>
           </UserProvider>
         </AuthProvider>
       </AdminProvider>
     </ArrayDatabase>
-  );
+  )
 }
 
-export default App;
+export default App
